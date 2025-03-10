4a607cdd3fe52142338d4d12c646f4e8bb4a9582
Facundo Malnero <facundomalnero010@gmail.com>
2025-03-10 01:44:44 +0000

    fix: corrects type hint

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -65,7 +65,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         });
     }
 
-    protected function authorization(): bool
+    protected function authorization(): void
     {
         $this->gate();
 
