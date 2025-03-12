b09dfb28dbac7fa4a9b48c3de960d0a8b27c3653
Facundo Malnero <facundomalnero010@gmail.com>
2025-03-12 02:34:49 +0000

    fix: ecs

--- Modules/Users/app/Http/Requests/UsersIndexRequest.php
+++ Modules/Users/app/Http/Requests/UsersIndexRequest.php
@@ -13,28 +13,28 @@ use Modules\Users\Enums\Statuses;
 
 class UsersIndexRequest extends FormRequest implements DataTransferableRequest
 {
-    private const string STATUS = 'status';
+    private const string STATUS     = 'status';
     private const string FIRST_NAME = 'firstName';
-    private const string LAST_NAME = 'lastName';
-    private const string EMAIL = 'email';
+    private const string LAST_NAME  = 'lastName';
+    private const string EMAIL      = 'email';
 
     public function rules(): array
     {
         return [
-            self::STATUS =>     ['nullable', new Enum(Statuses::class)],
+            self::STATUS     => ['nullable', new Enum(Statuses::class)],
             self::FIRST_NAME => ['nullable', 'string', new AlphaNumSpaces],
-            self::LAST_NAME =>  ['nullable', 'string', new AlphaNumSpaces],
-            self::EMAIL =>      ['nullable', 'string', new AlphaNumSpaces],
+            self::LAST_NAME  => ['nullable', 'string', new AlphaNumSpaces],
+            self::EMAIL      => ['nullable', 'string', new AlphaNumSpaces],
         ];
     }
 
     public function toDto(): UsersIndexRequestDto
     {
         return new UsersIndexRequestDto(
-            status: Statuses::tryFrom($this->string(self::STATUS)->toString()),
+            status:    Statuses::tryFrom($this->string(self::STATUS)->toString()),
             firstName: $this->string(self::FIRST_NAME)->toString(),
-            lastName: $this->string(self::LAST_NAME)->toString(),
-            email: $this->string(self::EMAIL)->toString(),
+            lastName:  $this->string(self::LAST_NAME)->toString(),
+            email:     $this->string(self::EMAIL)->toString(),
         );
     }
 }
