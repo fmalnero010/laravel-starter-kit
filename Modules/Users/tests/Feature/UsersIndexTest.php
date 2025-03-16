8470d702e36ed17d4ce4e37b4d065c7c61fa4dd3
Facundo Malnero <facundomalnero010@gmail.com>
2025-03-16 04:56:06 +0000

    test: captainhook test

--- Modules/Users/tests/Feature/UsersIndexTest.php
+++ Modules/Users/tests/Feature/UsersIndexTest.php
@@ -29,7 +29,7 @@ test('should return a list of users', function () {
                 'data' => [
                     '*' => $expectedUserCamelKeys,
                 ],
-                'meta' => [
+                'metasis' => [
                     'current_page',
                     'from',
                     'per_page',
